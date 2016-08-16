1b795b24f4d7fc618f52aa1d2acbe566b70dbfec
ljf1172361058 <1172361058@qq.com>
2016-08-16 14:19:05 +0800

    Update DBHelper.php
    PHP后端源码

--- PHP-Sign/DBHelper.php
+++ PHP-Sign/DBHelper.php
@@ -3,11 +3,11 @@
  * 初始化数据库相关参数
  * Author:lzh
  */
-$dbms='mysql';     //数据库类型
-$host='localhost'; //数据库主机名
-$dbName='sign_in';    //使用的数据库
-$user='root';      //数据库连接用户名
-$pwd='root';          //对应的密码
-$dsn="$dbms:host=$host;dbname=$dbName";
+$dbms = 'mysql';     //数据库类型
+$host = 'localhost'; //数据库主机名
+$dbName = 'sign_in';    //使用的数据库
+$user = 'root';      //数据库连接用户名
+$pwd = 'root';          //对应的密码
+$dsn = "$dbms:host=$host;dbname=$dbName";
 
 ?>
