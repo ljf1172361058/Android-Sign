2e13ab193f32da08d7e9e42e2b4bd1a66aa24da3
Lzh <1172361058@qq.com>
2016-10-13 16:22:46 +0800

    Update DBHelper.php

--- PHP-Sign/DBHelper.php
+++ PHP-Sign/DBHelper.php
@@ -3,11 +3,11 @@
  * 初始化数据库相关参数
  * Author:lzh
  */
-$dbms = 'mysql';     // 数据库类型
-$host = 'localhost'; // 数据库主机名
-$dbName = 'sign_in';    // 使用的数据库
-$user = 'root';      // 数据库连接用户名
-$pwd = 'root';          // 对应的密码
+$dbms = 'xxx';     // 数据库类型
+$host = 'xxx'; // 数据库主机名
+$dbName = 'xxx';    // 使用的数据库
+$user = 'xxx';      // 数据库连接用户名
+$pwd = 'xxx';          // 对应的密码
 $dsn = "$dbms:host=$host;dbname=$dbName";
 
 ?>
